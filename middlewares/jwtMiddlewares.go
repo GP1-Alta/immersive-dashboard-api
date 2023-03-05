3583d91fefb7473068a2d99accdab4495c615b00
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-05 11:55:20 +0700

    add jwt middleware

--- middlewares/jwtMiddlewares.go
+++ middlewares/jwtMiddlewares.go
@@ -1,1 +1,37 @@
 package middlewares
+
+import (
+	"immersive-dashboard/app/config"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+func JWTMiddleware() echo.MiddlewareFunc {
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte(config.SECRET_JWT),
+		SigningMethod: "HS256",
+	})
+}
+
+func CreateToken(userId uint, role string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = userId
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(config.SECRET_JWT))
+
+}
+
+func ExtractTokenUserId(e echo.Context) uint {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(float64)
+		return uint(userId)
+	}
+	return 0
+}

8ab2600e885898bfff3b119e2488cfd6cac58e83
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-05 23:27:49 +0700

    create users request struct

--- features/users/delivery/request.go
+++ features/users/delivery/request.go
@@ -1,1 +1,27 @@
 package delivery
+
+type RegisterReq struct {
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+	Team     string `json:"team" form:"team"`
+	Role     string `json:"role" form:"role"`
+	Status   string `json:"status" form:"status"`
+}
+
+type LoginReq struct {
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+type UpdateReq struct {
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Team     string `json:"team" form:"team"`
+	Status   string `json:"status" form:"status"`
+}
+
+type ChangePass struct {
+	OldPass string `json:"old_pass" form:"old_pass"`
+	NewPass string `json:"new_pass" form:"new_pass"`
+}

7b805d33a3a566c126c30f9e2ccddcf49310703b
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-05 17:24:36 +0700

    create migration table users, classes, logs, mentees

--- features/users/data/model.go
+++ features/users/data/model.go
@@ -1,1 +1,18 @@
 package data
+
+import (
+	_modelClass "immersive-dashboard/features/classes/data"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string `gorm:"unique"`
+	Password string
+	Role     string `gorm:"type:enum('Admission', 'Mentor', 'People Skill', 'Placement', 'Academic')"`
+	Team     string `gorm:"type:enum('Active', 'Not-Active', 'Deleted')"`
+	Status   string
+	Classes  []_modelClass.Class `gorm:"foreignKey:Mentor"`
+}

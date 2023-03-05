712ef50fc8dac92a3b9a1cd136e13add0255913b
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-05 23:25:47 +0700

    create users entity

--- features/users/entity.go
+++ features/users/entity.go
@@ -1,1 +1,25 @@
 package users
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	Id       uint
+	Name     string `validate:"required"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required"`
+	Role     string `validate:"required"`
+	Team     string `validate:"required"`
+	Status   string `validate:"required"`
+}
+
+type UserDelivery interface {
+	Register() echo.HandlerFunc
+}
+
+type UserService interface {
+	RegisterSrv(newUser Core) error
+}
+
+type UserData interface {
+	RegisterData(newUser Core) error
+}

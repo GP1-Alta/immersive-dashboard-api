71c80fde73721a3300105f1dd01bbcd1596c116b
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-05 11:52:13 +0700

    echo, logger middlewares, router skeleton

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"immersive-dashboard/app/config"
+	"immersive-dashboard/app/database"
+	"immersive-dashboard/app/router"
+	"immersive-dashboard/middlewares"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	cfg := config.InitConfig()
+	db := database.InitDBMySql(*cfg)
+	database.InitialMigration(db)
+
+	e := echo.New()
+	middlewares.LogMiddlewares(e)
+	router.InitRouter(db, e)
+	e.Logger.Fatal(e.Start(":8080"))
+}

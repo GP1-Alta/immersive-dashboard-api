3b6640eaa096dc69e9df681130b34cacd65ef5a4
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-06 11:48:41 +0700

    [wip] get mentees

--- features/mentees/delivery/response.go
+++ features/mentees/delivery/response.go
@@ -1,1 +1,11 @@
 package delivery
+
+type MenteeResponse struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Sex      string `json:"sex"`
+	Status   string `json:"status"`
+	ClassID  uint   `json:"class_id"`
+	Category string `json:"category"`
+}

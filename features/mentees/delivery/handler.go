8a4a1c6e08181cf695ec886f1350930674bab1fe
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-06 19:54:51 +0700

    add edit mentee handler

--- features/mentees/delivery/handler.go
+++ features/mentees/delivery/handler.go
@@ -4,6 +4,7 @@ import (
 	"immersive-dashboard/features/mentees"
 	"immersive-dashboard/utils/helper"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -30,4 +31,24 @@ func (delivery *MenteeHandler) CreateMentee(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, helper.Response("Failed Add Mentee, error: "+err.Error()))
 	}
 	return c.JSON(http.StatusOK, helper.Response("Success Add Mentee"))
+}
+
+func (delivery *MenteeHandler) Edit(c echo.Context) error {
+	id := c.Param("id")
+	idConv, errConv := strconv.Atoi(id)
+	if errConv != nil {
+		return c.JSON(http.StatusBadRequest, helper.Response("Failed Update Mentee, id param must number"))
+	}
+	menteeInput := MenteeRequest{}
+	errBind := c.Bind(&menteeInput)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, helper.Response("Failed Update Mentee, error bind data"))
+	}
+
+	dataCore := requestToCore(menteeInput)
+	err := delivery.menteeService.Edit(dataCore, uint(idConv))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.Response("Failed Update Mentee, error update data"))
+	}
+	return c.JSON(http.StatusOK, helper.Response("Success Update Mentee"))
 }

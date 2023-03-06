67db2b758163841c7efa7d5c0236dfb6c3575e25
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-06 09:49:09 +0700

    create interface data and service for mentees

--- features/mentees/entity.go
+++ features/mentees/entity.go
@@ -1,1 +1,29 @@
 package mentees
+
+import "time"
+
+type Core struct {
+	ID        uint
+	Email     string `validate:"required,email"`
+	Name      string `validate:"required"`
+	Sex       string `validate:"required"`
+	Status    string `validate:"required"`
+	ClassID   string `validate:"required"`
+	Category  string `validate:"required"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type MenteeDataInterface interface {
+	SelectAll() ([]Core, error)
+	Insert(input Core) error
+	Update(input Core, id uint) error
+	Delete(data Core, id uint) error
+}
+
+type MenteeServiceInterface interface {
+	GetAll() ([]Core, error)
+	Create(input Core) error
+	Edit(input Core, id uint) error
+	Delete(data Core, id uint) error
+}

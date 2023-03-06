a8bd34cce935347927612e0672403c5500dc7d81
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-06 22:18:00 +0700

    add entity for user update

--- features/users/entity.go
+++ features/users/entity.go
@@ -16,16 +16,19 @@ type UserDelivery interface {
 	Register() echo.HandlerFunc
 	Login() echo.HandlerFunc
 	GetUser() echo.HandlerFunc
+	UpdateUser() echo.HandlerFunc
 }
 
 type UserService interface {
 	RegisterSrv(newUser Core) error
 	LoginSrv(email, password string) (string, Core, error)
 	GetUser(int, string)([]Core, error)
+	UpdateUserSrv(id int, newUser Core) error
 }
 
 type UserData interface {
 	RegisterData(newUser Core) error
 	LoginData(email string) (Core, error)
 	GetUser(int, string)([]Core, error)
+	UpdateUserData(id int, newUser Core) error
 }

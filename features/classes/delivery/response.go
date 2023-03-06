41e2861cf2c695300bab1a9f3dbad0a0c0d8ef85
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-07 00:24:51 +0700

    classes: create core, model, request, and response struct

--- features/classes/delivery/response.go
+++ features/classes/delivery/response.go
@@ -1,1 +1,29 @@
 package delivery
+
+import "immersive-dashboard/features/classes"
+
+type ClassResponse struct {
+	ID           uint   `json:"id"`
+	Name         string `json:"name"`
+	Mentor       string `json:"mentor"`
+	StartDateStr string `json:"start_date"`
+	EndDateStr   string `json:"end_date"`
+}
+
+func coreToResponse(dataCore classes.Core) ClassResponse {
+	return ClassResponse{
+		ID:           dataCore.ID,
+		Name:         dataCore.Name,
+		Mentor:       dataCore.Mentor,
+		StartDateStr: dataCore.StartDateStr,
+		EndDateStr:   dataCore.EndDateStr,
+	}
+}
+
+func listCoreToResponse(dataCore []classes.Core) []ClassResponse {
+	var dataResponse []ClassResponse
+	for _, v := range dataCore {
+		dataResponse = append(dataResponse, coreToResponse(v))
+	}
+	return dataResponse
+}
--- features/classes/entity.go
+++ features/classes/entity.go
@@ -1,1 +1,16 @@
 package classes
+
+import "time"
+
+type Core struct {
+	ID           uint
+	Name         string `validate:"required,max=50"`
+	UserID       uint   `validate:"required"`
+	Mentor       string
+	StartDate    time.Time `validate:"required"`
+	StartDateStr string
+	EndDate      time.Time `validate:"required"`
+	EndDateStr   string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}

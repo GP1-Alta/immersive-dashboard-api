c973fba61fd478a683c94072b72c27e5058c97e3
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-06 09:50:42 +0700

    implement interface data mentee

--- features/mentees/data/query.go
+++ features/mentees/data/query.go
@@ -1,1 +1,37 @@
 package data
+
+import (
+	"immersive-dashboard/features/mentees"
+
+	"gorm.io/gorm"
+)
+
+type menteeQuery struct {
+	db *gorm.DB
+}
+
+// Delete implements mentees.MenteeDataInterface
+func (*menteeQuery) Delete(data mentees.Core, id uint) error {
+	panic("unimplemented")
+}
+
+// Insert implements mentees.MenteeDataInterface
+func (*menteeQuery) Insert(input mentees.Core) error {
+	panic("unimplemented")
+}
+
+// SelectAll implements mentees.MenteeDataInterface
+func (*menteeQuery) SelectAll() ([]mentees.Core, error) {
+	panic("unimplemented")
+}
+
+// Update implements mentees.MenteeDataInterface
+func (*menteeQuery) Update(input mentees.Core, id uint) error {
+	panic("unimplemented")
+}
+
+func New(db *gorm.DB) mentees.MenteeDataInterface {
+	return &menteeQuery{
+		db: db,
+	}
+}

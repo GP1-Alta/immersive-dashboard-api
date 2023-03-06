ea1ea33d74dad152dd73859dd1f479a83c86ff5f
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-06 09:55:19 +0700

    create mentee handler struct

--- features/mentees/delivery/handler.go
+++ features/mentees/delivery/handler.go
@@ -1,1 +1,13 @@
 package delivery
+
+import "immersive-dashboard/features/mentees"
+
+type MenteeHandler struct {
+	menteeService mentees.MenteeServiceInterface
+}
+
+func New(srv mentees.MenteeServiceInterface) *MenteeHandler {
+	return &MenteeHandler{
+		menteeService: srv,
+	}
+}

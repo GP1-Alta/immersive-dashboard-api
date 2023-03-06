79fecbdd88beea385b0a375d48ba833e4e74d9cc
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-06 14:41:23 +0700

    add struct mentee request

--- features/mentees/delivery/request.go
+++ features/mentees/delivery/request.go
@@ -1,1 +1,43 @@
 package delivery
+
+import "immersive-dashboard/features/mentees"
+
+type MenteeRequest struct {
+	Name            string `json:"name" form:"name"`
+	Address         string `json:"address" form:"address"`
+	HomeAddress     string `json:"home_address" form:"home_address"`
+	Email           string `json:"email" form:"email"`
+	Sex             string `json:"sex" form:"sex"`
+	Telegram        string `json:"telegram" form:"telegram"`
+	Phone           string `json:"phone" form:"phone"`
+	Discord         string `json:"discord" form:"discord"`
+	Status          string `json:"status" form:"status"`
+	Class           string `json:"class" from:"class"`
+	EmergencyName   string `json:"emergency_name" form:"emergency_name"`
+	EmergencyPhone  string `json:"emergency_phone" form:"emergency_phone"`
+	EmergencyStatus string `json:"emergency_status" form:"emergency_status"`
+	Category        string `json:"category" form:"category"`
+	Major           string `json:"major" form:"major"`
+	Institution     string `json:"institution" form:"institution"`
+}
+
+func requestToCore(dataRequest MenteeRequest) mentees.Core {
+	return mentees.Core{
+		Name:            dataRequest.Name,
+		Address:         dataRequest.Address,
+		HomeAddress:     dataRequest.HomeAddress,
+		Email:           dataRequest.Email,
+		Sex:             dataRequest.Sex,
+		Telegram:        dataRequest.Telegram,
+		Phone:           dataRequest.Phone,
+		Discord:         dataRequest.Discord,
+		StatusID:        0,
+		ClassID:         0,
+		EmergencyName:   dataRequest.EmergencyName,
+		EmergencyPhone:  dataRequest.Phone,
+		EmergencyStatus: dataRequest.Status,
+		Category:        dataRequest.Category,
+		Major:           dataRequest.Major,
+		Institution:     dataRequest.Institution,
+	}
+}

f041d4bdf8f59405ebdad95a7fc3ae3756ea81b3
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-07 10:42:59 +0700

    create get all status query

--- features/status/delivery/handler.go
+++ features/status/delivery/handler.go
@@ -1,1 +1,13 @@
 package delivery
+
+import "immersive-dashboard/features/status"
+
+type StatusHandler struct {
+	statusService status.StatusServiceInterface
+}
+
+func New(srv status.StatusServiceInterface) *StatusHandler {
+	return &StatusHandler{
+		statusService: srv,
+	}
+}
--- features/status/service/logic.go
+++ features/status/service/logic.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"immersive-dashboard/features/status"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type statusService struct {
+	statusData status.StatusDataInterface
+	validate   *validator.Validate
+}
+
+// GetAll implements status.StatusServiceInterface
+func (service *statusService) GetAll() ([]status.Core, error) {
+	panic("unimplemented")
+}
+
+func New(repo status.StatusDataInterface) status.StatusServiceInterface {
+	return &statusService{
+		statusData: repo,
+		validate:   validator.New(),
+	}
+}

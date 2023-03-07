1444cd0e6963d38c7bc41c076d352fd8ebfeebfc
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-07 09:29:21 +0700

    add entity for add log

--- features/logs/entity.go
+++ features/logs/entity.go
@@ -1,1 +1,23 @@
 package logs
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	Id       uint
+	MenteeID uint
+	UserID   uint
+	StatusID uint
+	Feedback string `validate:"required"`
+}
+
+type LogDelivery interface {
+	AddLog() echo.HandlerFunc
+}
+
+type LogService interface {
+	AddLogSrv(newLog Core) error
+}
+
+type LogData interface {
+	AddLogData(newLog Core) error
+}

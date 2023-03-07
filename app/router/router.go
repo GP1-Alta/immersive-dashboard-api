0cfa563e83dd5b4cbb04f84725085c77a10d00c5
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-07 10:46:51 +0700

    add get all status route

--- app/router/router.go
+++ app/router/router.go
@@ -7,16 +7,20 @@ import (
 	_menteeData "immersive-dashboard/features/mentees/data"
 	_menteeHandler "immersive-dashboard/features/mentees/delivery"
 	_menteeService "immersive-dashboard/features/mentees/service"
+	_statusData "immersive-dashboard/features/status/data"
+	_statusHandler "immersive-dashboard/features/status/delivery"
+	_statusService "immersive-dashboard/features/status/service"
+
+	_logData "immersive-dashboard/features/logs/data"
+	_logHandler "immersive-dashboard/features/logs/delivery"
+	_logService "immersive-dashboard/features/logs/service"
+	_userData "immersive-dashboard/features/users/data"
+	_userHandler "immersive-dashboard/features/users/delivery"
+	_userService "immersive-dashboard/features/users/service"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
-	_userData "immersive-dashboard/features/users/data"
-	_userHandler "immersive-dashboard/features/users/delivery"
-	_userService "immersive-dashboard/features/users/service"
-	_logData "immersive-dashboard/features/logs/data"
-	_logHandler "immersive-dashboard/features/logs/delivery"
-	_logService "immersive-dashboard/features/logs/service"
 )
 
 func InitRouter(db *gorm.DB, e *echo.Echo) {
@@ -38,11 +42,18 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	e.GET("/classes/list", classHandlerAPI.List)
 	e.PUT("/classes/:id", classHandlerAPI.Edit)
 	e.DELETE("/classes/:id", classHandlerAPI.Delete)
+
+	//classes
+	statusData := _statusData.New(db)
+	statusService := _statusService.New(statusData)
+	statusHandlerAPI := _statusHandler.New(statusService)
+	e.GET("/status", statusHandlerAPI.List)
+
 	v := validator.New()
 	userData := _userData.New(db)
 	userSrv := _userService.New(userData, v)
 	userHdl := _userHandler.New(userSrv)
-	
+
 	logData := _logData.New(db)
 	logSrv := _logService.New(logData, v)
 	logHdl := _logHandler.New(logSrv)

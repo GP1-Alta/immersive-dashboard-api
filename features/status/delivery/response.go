1a87b458c84b60250c192f97ec586ede290aad88
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-07 10:37:45 +0700

    create status response struct

--- features/status/delivery/response.go
+++ features/status/delivery/response.go
@@ -1,1 +1,23 @@
 package delivery
+
+import "immersive-dashboard/features/status"
+
+type StatusResponse struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
+
+func coreToResponse(dataCore status.Core) StatusResponse {
+	return StatusResponse{
+		ID:   dataCore.ID,
+		Name: dataCore.Name,
+	}
+}
+
+func listCoreToResponse(dataCore []status.Core) []StatusResponse {
+	var dataResponse []StatusResponse
+	for _, v := range dataCore {
+		dataResponse = append(dataResponse, coreToResponse(v))
+	}
+	return dataResponse
+}

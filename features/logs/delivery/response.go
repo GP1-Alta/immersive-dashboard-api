bea2355eaeb3ebccc17f328918a326db32ecea2d
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-08 01:18:19 +0700

    add response json for get log

--- features/logs/delivery/response.go
+++ features/logs/delivery/response.go
@@ -1,1 +1,12 @@
 package delivery
+
+type LogResponse struct {
+	ID         uint   `json:"id"`
+	UserName   string `json:"mentor"`
+	StatusName string `json:"name"`
+	Feedback   string `json:"fedback"`
+	CreatedAt  string `json:"date"`
+	Image      string `json:"image"`
+}
+
+type ListLogResponse []LogResponse

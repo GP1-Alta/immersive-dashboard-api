9f031ac617ea02fcf727e615453ef73e521733c7
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-03-07 13:05:11 +0700

    fix role default value

--- features/users/data/model.go
+++ features/users/data/model.go
@@ -15,7 +15,7 @@ type User struct {
 	Password string
 	Team     string `gorm:"type:enum('Admission', 'Mentor', 'People Skill', 'Placement', 'Academic')"`
 	Status   string `gorm:"type:enum('Active', 'Not-Active', 'Deleted')"`
-	Role     string `gorm:"type:varchar(10) not null default 'Default'"`
+	Role     string `gorm:"type:varchar(10);not null;default:Default"`
 	Classes  []_modelClass.Class
 	Logs     []_modelLog.Log
 }

06eb29d1bc4008438b6adea70c20bbfe7e81e173
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-07 10:45:03 +0700

    add get all status handler

--- features/status/delivery/handler.go
+++ features/status/delivery/handler.go
@@ -1,6 +1,12 @@
 package delivery
 
-import "immersive-dashboard/features/status"
+import (
+	"immersive-dashboard/features/status"
+	"immersive-dashboard/utils/helper"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 type StatusHandler struct {
 	statusService status.StatusServiceInterface
@@ -10,4 +16,13 @@ func New(srv status.StatusServiceInterface) *StatusHandler {
 	return &StatusHandler{
 		statusService: srv,
 	}
+}
+
+func (delivery *StatusHandler) List(c echo.Context) error {
+	data, err := delivery.statusService.GetAll()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.Response("Failed, error read data"))
+	}
+	dataResponse := listCoreToResponse(data)
+	return c.JSON(http.StatusOK, helper.ResponseWithData("Success", dataResponse))
 }

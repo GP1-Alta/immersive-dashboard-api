4715f1bf29d23d6f936040cb48042f1bde659db9
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-08 13:25:30 +0700

    unit test: select mentee by id

--- features/mentees/service/logic_test.go
+++ features/mentees/service/logic_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 var (
@@ -55,6 +56,69 @@ func TestDelete(t *testing.T) {
 		err := srv.Delete(mock_data_mentee, id)
 		assert.NotNil(t, err)
 		assert.Equal(t, "error delete data class", err.Error())
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestInsert(t *testing.T) {
+	repo := new(mocks.MenteeData)
+
+	t.Run("Success Insert", func(t *testing.T) {
+		repo.On("Insert", mock_data_mentee).Return(nil).Once()
+
+		srv := New(repo)
+		err := srv.Create(mock_data_mentee)
+		assert.Nil(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Failed when func Insert return error", func(t *testing.T) {
+		repo.On("Insert", mock_data_mentee).Return(errors.New("error insert data")).Once()
+
+		srv := New(repo)
+		err := srv.Create(mock_data_mentee)
+		assert.NotNil(t, err)
+		assert.Equal(t, "error insert data", err.Error())
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Failed validate", func(t *testing.T) {
+		inputData := mentees.Core{
+			ID:              1,
+			Name:            "Naruto Uzumaki",
+			Address:         "Konohagakure",
+			HomeAddress:     "Konohagakure",
+			Email:           "naruto@alta.com",
+			Sex:             "Male",
+			Telegram:        "@narutouzumaki",
+			Phone:           "08123456789",
+			Discord:         "naruto#5060",
+			StatusID:        1,
+			ClassID:         1,
+			EmergencyPhone:  "0123456789",
+			EmergencyStatus: "Orang Tua",
+			Category:        "Informatics",
+			Major:           "Teknik Informatika",
+			Institution:     "Institut Teknologi Konoha",
+		}
+		srv := New(repo)
+		err := srv.Create(inputData)
+		assert.NotNil(t, err)
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestSelect(t *testing.T) {
+	repo := new(mocks.MenteeData)
+	idMentee := uint(1)
+
+	t.Run("Success Select One", func(t *testing.T) {
+		repo.On("Select", mock.AnythingOfType("uint")).Return(mock_data_mentee, nil).Once()
+
+		srv := New(repo)
+		response, err := srv.Get(idMentee)
+		assert.Nil(t, err)
+		assert.Equal(t, mock_data_mentee.Name, response.Name)
 		repo.AssertExpectations(t)
 	})
 }

a3c24b782994818f9f8cf17e7a536c3c16c36d25
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-08 12:49:05 +0700

    unit test: insert class

--- features/classes/service/logic_test.go
+++ features/classes/service/logic_test.go
@@ -46,3 +46,39 @@ func TestDelete(t *testing.T) {
 		repo.AssertExpectations(t)
 	})
 }
+
+func TestInsert(t *testing.T) {
+	repo := new(mocks.ClassData)
+
+	t.Run("Success Insert Class", func(t *testing.T) {
+		repo.On("Insert", mock_data_class).Return(nil).Once()
+
+		srv := New(repo)
+		err := srv.Create(mock_data_class)
+		assert.Nil(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Failed when func Insert return error", func(t *testing.T) {
+		repo.On("Insert", mock_data_class).Return(errors.New("error insert data")).Once()
+
+		srv := New(repo)
+		err := srv.Create(mock_data_class)
+		assert.NotNil(t, err)
+		assert.Equal(t, "error insert data", err.Error())
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Failed validate", func(t *testing.T) {
+		inputData := classes.Core{
+			ID:        1,
+			Name:      "Immersive Back End 15",
+			StartDate: time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC),
+			EndDate:   time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC),
+		}
+		srv := New(repo)
+		err := srv.Create(inputData)
+		assert.NotNil(t, err)
+		repo.AssertExpectations(t)
+	})
+}
